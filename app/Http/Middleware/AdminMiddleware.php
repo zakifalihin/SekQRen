f8231adf6d968c2fa92fb08451eb7fb9ae073636
Zaki Falihin <zakifalihin05@gmail.com>
2026-01-08 18:17:55 +0800

    Resolv Route Https

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -1,38 +1,19 @@
 <?php
 
 namespace App\Http\Middleware;
+use Illuminate\Support\Facades\URL;
 
 use Closure;
-use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
-
-// class AdminMiddleware
-// {
-//     /**
-//      * Handle an incoming request.
-//      *
-//      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-//      */
-//     public function handle($request, Closure $next)
-//     {
-//         if ($request->user()->role !== 'admin') {
-//             return response()->json(['message' => 'Akses ditolak'], 403);
-//         }
-//         return $next($request);
-//     }
-// }
-
 
 use Illuminate\Support\Facades\Auth;
 
 class AdminMiddleware
 {
     public function handle($request, Closure $next)
-    {
-        if (Auth::check() && Auth::user()->role === 'admin') {
-            return $next($request);
-        }
-        return redirect()->route('admin.login');
+    {   
+        if (config('app.env') === 'production' || env('FORCE_HTTPS', false)) {
+        URL::forceScheme('https');
+    }
     }
 }
 
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
+        
         $middleware->alias([
             'admin' => \App\Http\Middleware\AdminMiddleware::class,
             'guru'  => \App\Http\Middleware\GuruMiddleware::class,
